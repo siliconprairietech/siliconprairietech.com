4f1d029e2b734e7b90bcff8d7cfb8f61f1fbf2ec
Geoffrey Arnold <geoffrey@geoffreyarnold.com>
2015-11-14 22:15:00 -0600

    Falling back to X-AppEngine-CityLatLong request header if location is not provided.

--- invite.php
+++ invite.php
@@ -5,22 +5,30 @@ require_once('config.php');
 use google\appengine\api\taskqueue\PushTask;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $data = [
-        'email' => $_REQUEST['email'],
-        'token' => SLACK_TOKEN,
-        'set_active' => true];
-    $context = ['http' => [
-        'method' => 'post',
-        'content' => http_build_query($data)]];
-    $result = file_get_contents('https://' . SLACK_URL . '/api/users.admin.invite', false, stream_context_create($context));
-    print_r($result);
-    // {"ok":true}
-    // {"ok":false,"error":"already_invited"}
+    $data = ['email' => $_REQUEST['email'], 'token' => SLACK_TOKEN, 'set_active' => true];
+    $context = ['http' => ['method' => 'post', 'content' => http_build_query($data)]];
+    $response = file_get_contents('https://' . SLACK_URL . '/api/users.admin.invite', false, stream_context_create($context));
 
-    // $_SERVER['HTTP_X_APPENGINE_CITYLATLONG']
-    $task = new PushTask('/add-pin', ['lat' => $_REQUEST['lat'], 'lng' => $_REQUEST['lng']]);
-    $task_name = $task->add('pin-queue');
-    print_r($task_name);
+    if ($response === false) {
+        // HTTP error
+    }
+    else {
+        $result = json_decode($response, true);
+
+        // {"ok":true}
+        if ($result['ok'] === true) {
+            list($appengine_latitude, $appengine_longitude) = explode(',', $_SERVER['HTTP_X_APPENGINE_CITYLATLONG']);
+
+            $latitude = $_REQUEST['latitude'] ?: $appengine_latitude;
+            $longitude = $_REQUEST['longitude'] ?: $appengine_longitude;
+
+            if ($latitude && $longitude) {
+                (new PushTask('/add-pin', ['latitude' => $latitude, 'longitude' => $longitude]))->add('pin-queue');
+            }
+        }
+        // {"ok":false,"error":"already_invited"}
+        else {
+            $error = $result['error'];
+        }
+    }
 }
-
-phpinfo();
