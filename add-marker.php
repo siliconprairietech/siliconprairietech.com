23fd5ad50a26455b756ce0cd8fb3323480dd17a8
Geoffrey Arnold <geoffrey@geoffreyarnold.com>
2015-11-16 20:21:26 -0600

    Setting the content-type when uploading `markers.js` to Google Cloud Storage.

--- add-marker.php
+++ add-marker.php
@@ -8,7 +8,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $markers = file_exists(MARKERS_JS) ? file_get_contents(MARKERS_JS) : '';
     $markers .= 'window.setTimeout(function(){new google.maps.Marker({map:map,position:new google.maps.LatLng(' . $latitude . ',' . $longitude . '),animation:google.maps.Animation.DROP});},' . rand(1, 2500) . ');';
-    file_put_contents(MARKERS_JS, $markers);
+
+    $context = ['gs' => ['Content-Type' => 'application/javascript']];
+    file_put_contents(MARKERS_JS, $markers, 0, stream_context_create($context));
 }
 else {
     http_response_code(405); // Method Not Allowed
