1b6fa5b50ad30a6fb78baae5f425a65e4ab493e9
Geoffrey Arnold <geoffrey@geoffreyarnold.com>
2015-11-24 20:51:19 -0600

    Increasing verbosity when pushing to App Engine.

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PATH="/opt/google_appengine:$PATH"
-appcfg.py -A silicon-prairie-tech update --noauth_local_webserver `pwd`
+appcfg.py -A silicon-prairie-tech update --noauth_local_webserver --verbose `pwd`
